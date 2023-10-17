0f9f49d857947e02c96a0d04408039c7fcc93b7c
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-10-17 16:19:29 -0700

    update files

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, jsonify
+import os
+import pgpy
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import logging
+
+# setup a logger
+log = logging.getLogger(__name__)
+log.setLevel(os.environ.get("LOG_LEVEL", "INFO").upper())
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+log.addHandler(handler)
+log.info("Starting Hush Line")
+
+app = Flask(__name__)
+
+sender_email = os.environ.get("EMAIL", None)
+sender_password = os.environ.get("NOTIFY_PASSWORD", None)
+smtp_server = os.environ.get("NOTIFY_SMTP_SERVER", None)
+smtp_port = int(os.environ.get("NOTIFY_SMTP_PORT", 0))
+
+if not sender_email or not sender_password or not smtp_server or not smtp_port:
+    log.warn(
+        "Missing email notification configuration(s). Email notifications will not be sent."
+    )
+
+# Load the public key into memory on startup
+with open("public_key.asc", "r") as key_file:
+    key_data = key_file.read()
+    PUBLIC_KEY, _ = pgpy.PGPKey.from_blob(key_data)  # Extract the key from the tuple
+
+def encrypt_message(message):
+    encrypted_message = str(PUBLIC_KEY.encrypt(pgpy.PGPMessage.new(message)))
+    return encrypted_message
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
+@app.route("/save_message", methods=["POST"])
+def save_message():
+    email = request.form["email"]
+    encrypted_email = encrypt_message(email)   # Change here
+    with open("messages.txt", "a") as f:
+        f.write(encrypted_email + "\n\n")     # Updated the variable name here as well
+    send_email_notification(encrypted_email)  # Updated the variable name here
+    return jsonify({"success": True})
+
+def send_email_notification(message):
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = sender_email
+    msg["Subject"] = "🤫 New Hush Line Message Received"
+
+    body = f"{message}"
+    msg.attach(MIMEText(body, "plain"))
+
+    try:
+        with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, sender_email, msg.as_string())
+    except Exception as e:
+        log.error(f"Error sending email notification: {e}")
+
+@app.route("/pgp_owner_info")
+def pgp_owner_info():
+    owner = f"{PUBLIC_KEY.userids[0].name}\n{PUBLIC_KEY.userids[0].email}"
+    key_id = f"Key ID: {str(PUBLIC_KEY.fingerprint)[-8:]}"
+    if PUBLIC_KEY.expires_at is not None:
+        expires = f"Exp: {PUBLIC_KEY.expires_at.strftime('%Y-%m-%d')}"
+    else:
+        expires = f"Exp: Never"
+    return jsonify({"owner_info": owner, "key_id": key_id, "expires": expires})
+
+if __name__ == "__main__":
+    app.run()
